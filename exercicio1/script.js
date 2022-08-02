e091b9c5294f297af23e6b40ba7375e04b8f3b72
danielmenna <danielmenna@gmail.com>
2022-08-01 21:14:30 -0300

    resolução do exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+const sacolao = []
+
+const item1 = {
+  nome: 'banana',
+  preco: 1.5,
+  estaDisponivel: true
+}
+
+const item2 = {
+  nome: 'maçã',
+  preco: 1.75,
+  estaDisponivel: true
+}
+
+const item3 = {
+  nome: 'uva',
+  preco: 7.5,
+  estaDisponivel: true
+}
+
+sacolao.push(item1, item2, item3)
+console.log('ITENS NA SACOLA: ', sacolao)
